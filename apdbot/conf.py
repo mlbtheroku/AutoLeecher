11e281613829e2d8f81c7fbe16c48ccabb7d75b9
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2023-01-04 10:14:54 +0630

    Update conf.py

--- apdbot/conf.py
+++ apdbot/conf.py
@@ -2,8 +2,8 @@ from apdbot.sample_con import Config
 #Fill your all data here Do not Edit other Files
 
 class Config(Config):
-    BOT_TOKEN= ""
-    APP_ID = 
-    API_HASH = ""
-    OWNER_ID = ""
-    AUTH = [556315181, -100088888, -10002252254]
+    BOT_TOKEN= "5845606249:AAH-CoS7NLQUOnI5JZvQcH6sNZIz79EnPA0"
+    APP_ID = 15830858
+    API_HASH = "2c015c994c57b312708fecc8a2a0f1a6"
+    OWNER_ID = "5884190716"
+    AUTH = [1349301822, -1001664467617, -1001750717073]
